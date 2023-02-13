8fa08f7d2dd82939349d957ac1a2faf68efca3bb
Giovanna Bezerra <giovanna.imeusp@gmail.com>
2023-02-13 13:51:25 -0300

    creating a loop to get data for each page with 50 movies listed

--- imdb_movie_getdata.py
+++ imdb_movie_getdata.py
@@ -1,18 +1,28 @@
-# Este programa coleta dados detalhados dos 250 filmes mais populares de acordo com a IMDB. Após o tratamento dos dados
-# a informação fica disponível e organizada para que sejam criados diversos tipos de visualizações.
+# This program scrape data from IMDB's "Top 250" movies. After collecting and processing data, the dataset is available for creating
+# various types of visualizations.
+
+# The information I gather from each movie are:
+# 1. The movie title
+# 2. The link to access movie page
+# 3. The year of release
+# 4. The age classification
+# 5. The genre of the movie
+# 6. How long the movie is
+# 7. IMDb’s rating of the movie
+# 8. The Metascore of the movie
+# 9. The movie summary
+# 10. The directors
+# 11. The star cast
+# 12. How many votes the movie got
+# 13. The U.S. gross earnings of the movie
+# 14. The image of movie poster
 
 # Import modules:
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# Building the soup constructor:
-url = 'https://www.imdb.com/search/title/?groups=top_250&sort=user_rating'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-
 # Creating empty lists to store values:
-indexes = []
 movies = []
 links = []
 years = []
@@ -25,125 +35,141 @@ summaries = []
 directors = []
 stars = []
 votes = []
-gross = []
-rank = []
+grosses = []
 images = []
 
-# Getting page 1:
+# There are 250 movies and each page has 50 movies listed.
+# Url1: 1-50, Url2: 51-100, Url3: 101-150, Url4: 151-200 and Url5: 201-250.
+# Let's create a lis of pages:
+
+urls = ['https://www.imdb.com/search/title/?groups=top_250',
+        'https://www.imdb.com/search/title/?groups=top_250&start=51&ref_=adv_nxt',
+        'https://www.imdb.com/search/title/?groups=top_250&start=101&ref_=adv_nxt',
+        'https://www.imdb.com/search/title/?groups=top_250&start=151&ref_=adv_nxt',
+        'https://www.imdb.com/search/title/?groups=top_250&start=201&ref_=adv_nxt'
+       ]
+
+# Creating request header config.:
 headers = {
     "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3538.102 Safari/537.36 Edge/18.19582"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3538.102 Safari/537.36 Edge/18.19582",
+    'Accept-Language': 
+    'en-US, en;q=0.5'
 }
-url = 'https://www.imdb.com/search/title/?groups=top_250&sort=user_rating'
-response = requests.get(url,headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
 
-# Splitting movies by content:
-movie_content = soup.find_all('div', class_ = 'lister-item mode-advanced')
-
-# Getting data for each content:
-for content in movie_content:
-    # Getting the index:
-    if content.h3.find('span', class_='lister-item-index') is not None:
-        index = content.h3.find('span', class_='lister-item-index').text.strip('.')
-        indexes.append(index)
-    else:
-        indexes.append("")
+for url in urls:
+    # Building the soup constructor:
+    response = requests.get(url,headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
     
-    # Getting Movie Title and Link:
-    if content.h3.a is not None:
-        movie = content.h3.a.text
-        movies.append(movie)
-        link = 'https://www.imdb.com'+ content.h3.a.get('href')
-        links.append(link)
-    else:
-        movies.append("")
-        links.append("")
+    # Splitting movies by content:
+    movie_content = soup.find_all('div', class_ = 'lister-item mode-advanced')
+    
+    # Getting data for each content:
+    for content in movie_content:
+
+        # Getting Movie Title and Link:
+        if content.h3.a is not None:
+            movie = content.h3.a.text
+            movies.append(movie)
+            link = 'https://www.imdb.com'+ content.h3.a.get('href')
+            links.append(link)
+        else:
+            movies.append("")
+            links.append("")
         
-    # Getting Year:
-    if content.h3.find('span', class_='lister-item-year') is not None:
-        year = content.h3.find('span', class_='lister-item-year').text.strip('()')
-        years.append(year)
-    else:
-        years.append("")
+        # Getting Year:
+        if content.h3.find('span', class_='lister-item-year') is not None:
+            year = content.h3.find('span', class_='lister-item-year').text.strip('()')
+            years.append(year)
+        else:
+            years.append("")
     
-    # Getting Certificate:
-    if content.p.find('span', class_ = 'certificate') is not None:
-        certificate = content.p.find('span', class_ = 'certificate').text
-        certificates.append(certificate)
-    else:
-        certificates.append("")
+        # Getting Certificate:
+        if content.p.find('span', class_ = 'certificate') is not None:
+            certificate = content.p.find('span', class_ = 'certificate').text
+            certificates.append(certificate)
+        else:
+            certificates.append("")
     
-    # Getting Run Time:
-    if content.p.find('span', class_='runtime') is not None:
-        runtime = content.p.find('span', class_='runtime').text
-        runtimes.append(runtime)
-    else:
-        runtimes.append(runtime)
-    
-    # Getting Genre:
-    if content.p.find('span', class_='genre') is not None:
-        genre = content.p.find('span', class_='genre').text.strip()
-        genres.append(genre)
-    else:
-        genres.append("")
-    
-    # Getting Rate:
-    if content.strong is not None:
-        rate = content.strong.text
-        rates.append(rate)
-    else:
-        rates.append("")
-    
-    # Getting Metascore:
-    if content.find('span', class_='metascore favorable') is not None:
-        metascore = content.find('span', class_='metascore favorable').text
-        metascores.append(metascore)
-    else:
-        metascores.append("")
-        
-    # Getting Summary:
-    if content.find_all('p', class_='text-muted')[1] is not None:
-        summary = content.find_all('p', class_='text-muted')[1].text.strip()
-        summaries.append(summary)
-    else:
-        summaries.append("")
-    
-    # Getting Directors:
-    if content.select('a[href*="_dr_"]') is not None:
-        director = [x.text for x in content.select('a[href*="_dr_"]')]
-        directors.append(director)
-    else:
-        directors.append("")
-    
-    # Getting Stars:
-    if content.select('a[href*="_st_"]') is not None:
-        star = [x.text for x in content.select('a[href*="_st_"]')]
-        stars.append(star)
-    else:
-        stars.append("")
+        # Getting Run Time:
+        if content.p.find('span', class_='runtime') is not None:
+            runtime = content.p.find('span', class_='runtime').text
+            runtimes.append(runtime)
+        else:
+            runtimes.append(runtime)
 
+        # Getting Genre:
+        if content.p.find('span', class_='genre') is not None:
+            genre = content.p.find('span', class_='genre').text.strip()
+            genres.append(genre)
+        else:
+            genres.append("")
 
-# Votes:
-#conteudo.select('p[class~=sort-num_votes-visible] span[name~=nv]')[0]
+        # Getting Rate:
+        if content.strong is not None:
+            rate = content.strong.text
+            rates.append(rate)
+        else:
+            rates.append("")
 
-# Gross:
-#conteudo.select('p[class~=sort-num_votes-visible] span[name~=nv]')[1]
+        # Getting Metascore:
+        if content.find('span', class_='metascore favorable') is not None:
+            metascore = content.find('span', class_='metascore favorable').text
+            metascores.append(metascore)
+        else:
+            metascores.append("")
 
-# Rank:
-#conteudo.select('p[class~=sort-num_votes-visible] span[name~=nv]')[2]
+        # Getting Summary:
+        if content.find_all('p', class_='text-muted')[1] is not None:
+            summary = content.find_all('p', class_='text-muted')[1].text.strip()
+            summaries.append(summary)
+        else:
+            summaries.append("")
 
+        # Getting Directors:
+        if content.select('a[href*="_dr_"]') is not None:
+            director = [x.text for x in content.select('a[href*="_dr_"]')]
+            directors.append(director)
+        else:
+            directors.append("")
 
-# Images:
-#conteudo.select('div.lister-item-image a')
+        # Getting Stars:
+        if content.select('a[href*="_st_"]') is not None:
+            star = [x.text for x in content.select('a[href*="_st_"]')]
+            stars.append(star)
+        else:
+            stars.append("")
 
-df = pd.DataFrame({'index': indexes,'movie_title': movies,
-                   'link': links,'year': years,
+        # Getting Votes:
+        if content.select('p[class~=sort-num_votes-visible] span[name~=nv]')[0] is not None:
+            vote = content.select('p[class~=sort-num_votes-visible] span[name~=nv]')[0].text
+            votes.append(vote)
+        else:
+            votes.append("")
+
+        # Getting Gross Value:
+        if content.select('p[class~=sort-num_votes-visible] span[name~=nv]')[1] is not None:
+            gross = content.select('p[class~=sort-num_votes-visible] span[name~=nv]')[1].text
+            grosses.append(gross)
+        else:
+            grosses.append("")
+
+        # Gettting Images:
+        if content.img.get('loadlate') is not None:
+            image = content.img.get('loadlate')
+            images.append(image)
+        else:
+            images.append("")
+
+# Creating the data frame:
+df = pd.DataFrame({'movie_title': movies,'link': links,'year': years,
                    'certificate': certificates,'runtime': runtimes,
-                   'genre': genres,'rate': rates,
-                   'metascore':metascores,'summary':summaries,
-                   'directors':directors,'stars':stars})
+                   'genre': genres,'rate': rates,'metascore':metascores,
+                   'summary':summaries,'directors':directors,'stars':stars,
+                   'votes':votes,'gross':grosses,'image':images})
 
+print(df.shape)
 print(df.head())
 
 
