2fb7d6a10d2f8fc8a175adcde15b748a558d8732
Giovanna Bezerra <giovanna.imeusp@gmail.com>
2023-03-01 16:28:29 -0300

    including charts Rating x Votes and Release date x Gross

--- imdb_movie_dash.py
+++ imdb_movie_dash.py
@@ -28,34 +28,132 @@ exec(open('imdb_movie_getdata.py').read())
 app = Dash(__name__,external_stylesheets=[dbc.themes.DARKLY])
 
 
+fig_rating_votes = go.Figure()
+
+fig_rating_votes.add_trace(
+    go.Scatter(
+        x=df.movie_title, y=df.votes.loc[0:20].sort_values(),
+        name = 'Votes',
+        mode = 'lines',
+        line = dict(width=0.5, color='blue'),
+        stackgroup = 'one'
+    ))
+
+
+fig_year_gross = go.Figure()
+
+df.gross = df.gross.fillna(0)
+df = df.sort_values('gross')
+
+fig_year_gross.add_trace(
+    go.Scatter(
+        x=df.year, y=df.movie_title,
+        text = df.gross,
+        hovertemplate = '<b>Gross:</b> $ %{text}M' + '<br><b>Year:</b> %{x}' + '<br><b>Title:<b> %{y}',
+        mode = 'markers',
+        marker = dict(color = df.gross, 
+                      size = df.gross, 
+                      sizemode = 'area', 
+                      sizeref = 2.*max(df.gross)/(50.**2),
+                      showscale=True)
+    ))
+
+fig_year_gross.update_xaxes(nticks=20)
+fig_year_gross.update_yaxes(visible=False)
+
+fig_year_gross.update_layout(
+    margin=dict(l=20, r=20, t=20, b=20),
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='SlateGrey',
+    autosize = False,
+    width = 600,
+    height = 400,
+    font_family = 'Arial',
+    font_color = 'White',
+    font_size = 14
+)
+
+
+
+#tabs_styles = {'height': '4vh', 'width': '12vw', "background": "#323130",
+#               'border': 'grey', 'border-radius': '4px', 'border-style': 'solid', 'border-color': 'grey'}
+
+
+tabs_styles = {
+    'height': '32px', 
+    'width': '500px',
+    'border-radius': '4px'
+}
+
+tab_style = {
+    "background": "	#133955",
+    'border-style': 'solid',
+    'border-color': 'grey',
+    'border-radius': '4px',
+    'align-items': 'center',
+    'padding': '5px',
+    'fontWeight': 'bold'
+}
+
+tab_selected_style = {
+    'backgroundColor': 'SlateGrey',
+    'border-style': 'solid',
+    'border-color': 'grey',
+    'border-radius': '4px',
+    'align-items': 'center',
+    'color': 'white',
+    'padding': '5px'
+}
+
+
 # Creating lay out:
 
 app.layout = dbc.Container(html.Div(
-    [dbc.Row(html.Div([
-                        html.H1('IMDB MOVIE DASHBOARD'),
-                        html.Hr(),
-                        html.P('Um dashboard para análise e consulta de filmes avaliados no IMDB'),
-                        html.Br()
-                        ]
-                        )
+    [dbc.Row([dbc.Col(html.Div([
+                                html.H1('IMDB MOVIE DASHBOARD'),
+                                html.Hr(),
+                                html.P('Um dashboard para análise e consulta de filmes avaliados no IMDB'),
+                                html.Br()
+                                ]
+                                ),width=10
+                    ),
+              dbc.Col(html.Div([
+                                html.Img(src='https://cdn-icons-png.flaticon.com/512/4618/4618701.png',
+                                         alt='fig-header',width=90 ,height=90
+                                         )
+                                ]
+                                ),width=2
+                    )
+            ]
             ),
      dbc.Row([dbc.Col(html.Div([
                                 html.H2('RATING X VOTES'),
                                 html.Hr(),
+                                dcc.Graph(figure=fig_rating_votes)
                                 ]
                                 )
                     ),
               dbc.Col(html.Div([
                                 html.H2('TOPS ON TABS'),
-                                html.Hr()
+                                html.Hr(),
+                                dcc.Tabs([
+                                    dcc.Tab(label='Tab one', children=[
+                                        html.H3('Conteúdo da tabela um')
+                                        ], style = tab_style, selected_style=tab_selected_style),
+                                    dcc.Tab(label='Tab two', children=[
+                                        html.H3('Conteúdo da tabela dois')
+                                        ], style = tab_style, selected_style=tab_selected_style)
+                                    ], style = tabs_styles)
                                 ]
                                 )
                     )
             ]
-            ),
+            ),html.Br(),
      dbc.Row([dbc.Col(html.Div([
                                 html.H2('RELEASE DATE X GROSS'),
-                                html.Hr()
+                                html.Hr(),
+                                dcc.Graph(figure=fig_year_gross),
+                                dcc.Slider(1920, 2030, 20, value = 1920)
                                 ]
                                 )
                     ),
