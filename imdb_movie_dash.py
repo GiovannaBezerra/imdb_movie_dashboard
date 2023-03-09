63edf2bd92b9dd2e4d98436838bc2b646c5dc2d8
Giovanna Bezerra <giovanna.imeusp@gmail.com>
2023-03-09 10:32:56 -0300

    including callback between graphs -Rating x Votes and Release Data x Gross- and Range sliders

--- imdb_movie_dash.py
+++ imdb_movie_dash.py
@@ -20,104 +20,25 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
+from dash_bootstrap_templates import load_figure_template
 
 # Getting data:
 exec(open('imdb_movie_getdata.py').read())
 
+# Incluir no file de getdata:
+df.gross = df.gross.fillna(0)
+
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 
 # App Create:
-app = Dash(__name__,external_stylesheets=[dbc.themes.DARKLY])
+app = Dash(__name__,external_stylesheets=[dbc.themes.DARKLY,dbc_css])
 
-# Gráfico Rating x Votes:
-df2=df.sort_values('votes')
-df2=df2.reset_index(drop=True)
-
-fig_rate_votes=make_subplots(specs=[[{"secondary_y": True}]])
-
-fig_rate_votes.add_trace(
-    go.Scatter(x=df2.movie_title,
-               y=df2.votes, 
-               name='Votes', 
-               mode='lines', 
-               hovertemplate=None,
-               line=dict(width=0.5, color='#325d81'),
-               stackgroup='one',
-               opacity=0.8),
-    secondary_y=True,
-)
-
-
-fig_rate_votes.add_trace(
-    go.Scatter(x=df2.movie_title,
-               y=df2.rate, 
-               name='Rate', 
-               mode='lines',
-               hovertemplate=None,
-               line=dict(width=0.5, color='#396a93'),
-               stackgroup='one'
-               ),
-    secondary_y=False,
-)
-
-fig_rate_votes.update_layout(
-    margin=dict(l=20, r=20, t=20, b=20),
-    paper_bgcolor='rgba(0,0,0,0)', 
-    plot_bgcolor='#f2f2f2',
-    autosize = False,
-    width = 600,
-    height = 400,
-    font_family = 'Arial',
-    font_color = 'White',
-    font_size = 14,
-    hovermode="x unified",
-    hoverlabel=dict(font_color='gray'),
-    legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
-)
-
-fig_rate_votes.update_xaxes(visible=False)
-fig_rate_votes.update_yaxes(title_text="<b>Rates</b>", secondary_y=False)
-fig_rate_votes.update_yaxes(title_text="<b>Votes</b>", secondary_y=True)
-
-# Grafico Year x Gross:
-fig_year_gross = go.Figure()
-
-df.gross = df.gross.fillna(0)
-df3 = df.sort_values('gross')
-
-fig_year_gross.add_trace(
-    go.Scatter(
-        x=df3.year, y=df3.movie_title,
-        text = df.gross,
-        hovertemplate = '<b>Gross:</b> $ %{text}M' + '<br><b>Year:</b> %{x}' + '<br><b>Title:<b> %{y}',
-        mode = 'markers',
-        marker = dict(color = df3.gross, 
-                      size = df3.gross, 
-                      sizemode = 'area', 
-                      sizeref = 2.*max(df3.gross)/(50.**2),
-                      showscale=True)
-    ))
-
-fig_year_gross.update_xaxes(nticks=20)
-fig_year_gross.update_yaxes(visible=False)
-
-fig_year_gross.update_layout(
-    margin=dict(l=20, r=20, t=20, b=20),
-    paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='SlateGrey',
-    autosize = False,
-    width = 600,
-    height = 400,
-    font_family = 'Arial',
-    font_color = 'White',
-    font_size = 14
-)
+load_figure_template("darkly")
 
 # Tabs styles:
-
 tabs_styles = {
-    'height': '32px', 
-    'width': '500px',
+   'height': '32px', 
+   'width': '500px',
     'align-items': 'center',
     'border-radius': '4px'
 }
@@ -143,10 +64,12 @@ tab_selected_style = {
 }
 
 
+
 # Creating lay out:
 
 app.layout = dbc.Container(html.Div(
     [dbc.Row([dbc.Col(html.Div([
+                                html.Br(),
                                 html.H1('IMDB MOVIE DASHBOARD'),
                                 html.Hr(),
                                 html.P('Um dashboard para análise e consulta de filmes avaliados no IMDB'),
@@ -164,37 +87,41 @@ app.layout = dbc.Container(html.Div(
             ]
             ),
      dbc.Row([dbc.Col(html.Div([
-                                html.H2('RATING X VOTES'),
+                                html.H4('RATING X VOTES'),
                                 html.Hr(),
-                                dcc.Graph(figure=fig_rate_votes),
-                                dcc.Slider(0, 250, 50, value = 50, className="mt-4")
+                                dcc.Graph(id='graph_rate_votes'),
+                                html.Br(),
+                                dcc.RangeSlider(8.0,9.4,0.2,value=[8.8,9.4],id='slider_rate_votes',className='dbc')
                                 ]
-                                )
+                                ),width=6
                     ),
               dbc.Col(html.Div([
-                                html.H2('TOPS ON TABS'),
+                                html.H4('RELEASE DATE X GROSS'),
                                 html.Hr(),
-                                dcc.Tabs([
-                                    dcc.Tab(label='Tab one', children=[
-                                        html.H3('Conteúdo da tabela um')
-                                        ], style = tab_style, selected_style=tab_selected_style),
-                                    dcc.Tab(label='Tab two', children=[
-                                        html.H3('Conteúdo da tabela dois')
-                                        ], style = tab_style, selected_style=tab_selected_style),
-                                    dcc.Tab(label='Tab three', children=[
-                                        html.H3('Conteúdo da tabela três')
-                                        ], style = tab_style, selected_style=tab_selected_style)
-                                    ], style = tabs_styles)
+                                dcc.Graph(id='graph_year_gross'),
+                                html.Br(),
+                                dcc.RangeSlider(1920,2030,10,value=[1960,2010],id='slider_year_gross',className='dbc',
+                                                marks={i: '{}'.format(i) for i in range(1920,2030,10)})
                                 ]
-                                )
+                                ),width=6
                     )
             ]
             ),html.Br(),
      dbc.Row([dbc.Col(html.Div([
-                                html.H2('RELEASE DATE X GROSS'),
+                                html.H4('TOPS ON TABS'),
                                 html.Hr(),
-                                dcc.Graph(figure=fig_year_gross),
-                                dcc.Slider(1920, 2030, 20, value = 1920)
+                                dcc.Tabs([
+                                    dcc.Tab(label='Tab one', children=[
+                                        html.H3('Conteúdo da tabela um'),
+                                        dcc.Graph(id='graph1')
+                                        ],style = tab_style, selected_style=tab_selected_style),
+                                    dcc.Tab(label='Tab two', children=[
+                                        html.H3('Conteúdo da tabela dois')
+                                        ],style = tab_style, selected_style=tab_selected_style),
+                                    dcc.Tab(label='Tab three', children=[
+                                        html.H3('Conteúdo da tabela três')
+                                        ],style = tab_style, selected_style=tab_selected_style)
+                                    ], style = tabs_styles)
                                 ]
                                 )
                     ),
@@ -207,8 +134,44 @@ app.layout = dbc.Container(html.Div(
             ]
             )
 ]
-), className="dbc"
+),className='dbc'
 )
+
+# Update RATING X VOTES graph:
+@app.callback(
+    Output('graph_rate_votes', 'figure'), 
+    Input('slider_rate_votes', 'value'))
+def update_graph_rate_votes(value):
+    df_votes = df[(df.rate>=value[0]) & (df.rate<=value[1])].sort_values('votes')
+    subfig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig_line = px.line(df_votes, x='movie_title', y='rate', template='darkly')
+    fig_area = px.area(df_votes, x='movie_title', y='votes', template='darkly')
+
+    fig_line.update_traces(hovertemplate='<b>Rate:</b> %{y}', line_color='#FF7F50', line_width=1)
+    fig_area.update_traces(yaxis="y2", hovertemplate='<b>Votes:</b> %{y:.0f}')
+
+    subfig.add_traces(fig_line.data + fig_area.data)
+    subfig.layout.yaxis.title='Rates'
+    subfig.layout.yaxis2.title='Votes'
+    subfig.update_xaxes(visible=False)
+    subfig.update_layout(hovermode='x unified')
+
+    return subfig
+
+
+# Update RELEASE DATE X GROSS graph:
+@app.callback(
+    Output('graph_year_gross', 'figure'), 
+    Input('slider_year_gross', 'value'))
+def update_graph_year_gross(value):
+    df_gross = df[(df.year>=value[0]) & (df.year<=value[1])].sort_values('gross')
+    fig = px.scatter(df_gross, x='year', y='gross', 
+                     color='gross', size='gross', template="darkly", custom_data=['movie_title'])
+    fig.update_traces(hovertemplate='<b>Movie: %{customdata[0]}</b>'+'<br><b>Gross:</b> $ %{y}M'+'<br><b>Year:</b> %{x}')
+    return fig
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
